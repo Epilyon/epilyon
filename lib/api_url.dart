d68c0a9e65b340abca66749cad9ccfc6c8a38467
kr4xkan <kr4xkan@gmail.com>
2021-11-13 12:36:07 +0100

    change api url to epilyon.krax.fr

--- lib/api_url.dart
+++ lib/api_url.dart
@@ -2,4 +2,4 @@
 // This is the simplest way to do it, just don't push your changes of this file to git
 // IOS Emulator: localhost
 // Android Emulator: 10.0.2.2
-const API_URL = 'http://10.0.2.2:7899';
+const API_URL = 'https://epilyon.krax.fr';
